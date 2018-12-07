515a84402fb9b5c94c7489fc1a51eba21a1d3dcd
Alexander Schneiders <a_schneide13@cs.uni-kl.de>
2018-12-08 00:57:27 +0100

    test List.fold on applying moves and detection of en passent

--- test.fs
+++ test.fs
@@ -4,9 +4,14 @@ open RookieOne.Engine
 let main args =
     let state = State.createDefault
     
-    let move = Move.createReadable White Pawn 'E' 2 'E' 4
+    let moves = [
+        Move.createReadable White Pawn 'E' 2 'E' 4
+        Move.createReadable White Pawn 'E' 4 'E' 5
+        Move.createReadable Black Pawn 'D' 7 'D' 5
+        Move.createReadable White Pawn 'E' 5 'D' 6
+    ]
 
-    let newState = State.applyMove state move
+    let newState = List.fold State.applyMove state moves
     printf  "%O" state
     printfn "#############################"
     printf  "%O" newState
